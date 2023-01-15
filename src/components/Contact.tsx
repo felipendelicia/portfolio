f03507f52d9a3bf6273239b989616b969b8a4f3a
felipendelicia <delicia4581@gmail.com>
2023-01-15 11:04:38 -0300

    contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,41 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin, faReact } from "@fortawesome/free-brands-svg-icons";
+import { faInbox, faFlag } from "@fortawesome/free-solid-svg-icons";
+import '../styles/Contact.css'
+
 export default function Contact() {
   return (
-    <div className="contact-component-container content">Contact</div>
-  )
+    <div className="contact-component-container content" id="contact">
+      <h1>Contacto</h1>
+      <hr />
+      <h3>Hablemos</h3>
+      <p>
+        <FontAwesomeIcon icon={faLinkedin} /> Linkedin{" "}
+        <a
+          href="https://www.linkedin.com/in/felipe-nahuel-delicia-b2487119a/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          in/felipe-nahuel-delicia-b2487119a
+        </a>
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faInbox} /> Email{" "}
+        <a href="mailto:delicia4581@gmail.com" target="_blank" rel="noreferrer">
+          delicia4581@gmail.com
+        </a>
+      </p>
+      <p>
+        <FontAwesomeIcon icon={faGithub} /> Github{" "}
+        <a href="mailto:delicia4581@gmail.com" target="_blank" rel="noreferrer">
+          github.com/felipendelicia
+        </a>
+      </p>
+      <h4>powered by</h4>
+      <div className="tech-contact">
+        <span className="react"><FontAwesomeIcon icon={faReact} /> React.js</span>
+        <span className="typescript"><FontAwesomeIcon icon={faFlag} /> Typescript</span>
+      </div>
+    </div>
+  );
 }
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,16 +1,18 @@
 import '../styles/Content.css'
 import About from './About';
+import Contact from './Contact';
 import Formation from './Formation';
 import Landing from './Landing';
 import Skills from './Skills';
 
 function Content() {
   return (
-  <div className="content-component-container">
+  <div className="content-component-container" id='contact'>
     <Landing/>
     <About/>
     <Formation/>
     <Skills/>
+    <Contact/>
   </div>
   )
 }
